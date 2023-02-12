14e374e797e59efe60b5eb361c88e187de94518a
Heechul Kim <jijisa@iorchard.co.kr>
2023-02-12 15:27:07 +0900

    add version information in --env parameter

--- build/drbd/run.sh
+++ build/drbd/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+DRBD_VER=${1:-9.2.2}
+DRBD_UTILS_VER=${2:-9.23.0}
 
+mkdir -p output
 docker build -t drbd-builder .
-docker run -v output:/output --rm drbd-builder
+docker run -v $(pwd)/output:/output --rm \
+  --env="DEBIAN_FRONTEND=noninteractive" \
+  --env="DRBD_VER=${DRBD_VER}" \
+  --env="DRBD_UTILS_VER=${DRBD_UTILS_VER}" \
+  drbd-builder
